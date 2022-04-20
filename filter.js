ff6bc38dd198a2d5aebdc804347c431dfdfcfaee
Josias Santos <83256416+josiassantos15@users.noreply.github.com>
2022-04-20 16:09:41 -0300

    Update filter.js
    Filtro exemplo 2

--- filter.js
+++ filter.js
@@ -1,4 +1,5 @@
-const arrayExemple = [[
+// Exemplo 1
+const arrayExemple = [
 			{
 				"id": "1",
 				"nome": "God of war",
@@ -9,12 +10,23 @@ const arrayExemple = [[
 				"nome": "Leon Resident",
 				"Age": "33"
 			},
-      {
+	      		{
 				"id": "1",
 				"nome": "God of war",
 				"Age": "30"
 			}]
+
   const ids = arrayExemple.map(item => item.id)
   const arrayFiltered = arrayExemple.filter(({id}, index) => !ids.includes(id, index + 1))
 
   console.log(arrayFiltered)
+
+
+// Exemplo 2
+const arrayExemple = ['Blue', 'Red', 'Pink', 'Blue'];
+
+const ArrayFiltered = [...new Set(array)];
+
+console.log(ArrayFiltered); 
+
+// ['Blue', 'Red', 'Pink']
